191d3265f8c2ee5bdae8063d7e7ebb2e91eec840
unknown <you@example.com>
2025-11-02 09:33:13 +0530

    Backend feature implemented

--- backend.py
+++ backend.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 class MultilingualChatbot:
     def __init__(self):
-        api_key = os.getenv("GROQ_API_KEY")
+        api_key = os.getenv("GROQ_API_KEY")#Api key
         if not api_key:
             raise ValueError("GROQ_API_KEY not found in environment variables")
         self.client = Groq(api_key=api_key)
